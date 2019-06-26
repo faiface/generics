3039396f75c4ec27d619979fbda041fba8afb029
faiface <faiface2202@gmail.com>
2019-06-27 01:10:57 +0200

    add a mathutils example

---
+++ examples/mathutils/mathutils.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+// Min returns the smaller one of the two.
+func Min(x, y type T ord) T {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// Max returns the bigger one of the two.
+func Max(x, y type T ord) T {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// Sum returns the sum of the numbers.
+func Sum(nums ...type T num) T {
+	result := T(0)
+	for _, x := range nums {
+		result += x
+	}
+	return result
+}
+
+// Product returns the product of the numbers.
+func Product(nums ...type T num) T {
+	result := T(1)
+	for _, x := range nums {
+		result *= x
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(Min(7, 9))
+	fmt.Println(Min(int32(10), 93))
+	fmt.Println(Max(3.14, 31.4))
+
+	fmt.Println(Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+	fmt.Println(Product(1, 2, 3, 4, 5))
+
+	bytes := ([]byte)("Hello, world!")
+	fmt.Println(Sum(bytes...))
+}
---
+++ examples/mathutils/out.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func Min_int(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Min_int32(x, y int32) int32 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Max_float64(x, y float64) float64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Sum_int(nums ...int) int {
+	result := int(0)
+	for _, x := range nums {
+		result += x
+	}
+	return result
+}
+
+func Product_int(nums ...int) int {
+	result := int(1)
+	for _, x := range nums {
+		result *= x
+	}
+	return result
+}
+
+func Sum_byte(nums ...byte) byte {
+	result := byte(0)
+	for _, x := range nums {
+		result += x
+	}
+	return result
+}
+func main() {
+	fmt.Println(Min_int(7, 9))
+	fmt.Println(Min_int32(int32(10), 93))
+	fmt.Println(Max_float64(3.14, 31.4))
+
+	fmt.Println(Sum_int(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+	fmt.Println(Product_int(1, 2, 3, 4, 5))
+
+	bytes := ([]byte)("Hello, world!")
+	fmt.Println(Sum_byte(bytes...))
+}
